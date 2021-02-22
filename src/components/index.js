e6a3f9976a4fe2a2edf23d66e9c47cd5f1c7a93a
muhammad wasi81 <wasiarain81@gmail.com>
2021-02-23 02:01:58 +0500

    Covid-19 Tracker App

---
+++ src/components/index.js
@@ -0,0 +1,3 @@
+export { default as Cards} from './Cards/Cards.jsx';
+export { default as Chart} from './Chart/Chart.jsx';
+export { default as CountryPicker} from './CountryPicker/CountryPicker.jsx';
